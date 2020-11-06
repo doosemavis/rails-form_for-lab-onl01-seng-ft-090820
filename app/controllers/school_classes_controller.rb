ccd2c67e610397fd1a233df64eb49133951c1ae8
doosemavis <moosedavis2011@gmail.com>
2020-11-06 14:37:40 -0600

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,19 @@
+class SchoolClasses < ApplicationController
+
+
+    def new
+    end 
+
+    def create
+    end 
+
+    def show
+    end 
+
+    def edit
+    end 
+
+    def update
+    end 
+    
+end 
