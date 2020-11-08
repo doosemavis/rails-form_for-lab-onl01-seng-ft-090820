76ab21eb52142becc5d8ba931f35fbc493823ca0
doosemavis <moosedavis2011@gmail.com>
2020-11-08 13:32:34 -0600

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -2,18 +2,26 @@ class SchoolClasses < ApplicationController
 
 
     def new
+        @school_class = SchoolClass.new
     end 
 
     def create
+        @school_class = SchoolClass.create(params.require(:school_class))
+        redirect_to school_class_path(@school_class)
     end 
 
     def show
+        @school_class = SchoolClass.find_by(id: params[:id])
     end 
 
     def edit
+        @school_class = SchoolClass.find_by(id: params[:id])
     end 
 
     def update
+        @school_class = SchoolClass.find_by(id: params[:id])
+        @school_class.update(params.require(:school_class))
+        redirect_to school_class_path(@school_class)
     end 
     
 end 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,18 +2,25 @@ class Students < ApplicationController
 
 
     def new
+        @student = Student.new
     end 
 
     def create
+        @student = Student.create(params.require(:student))
     end 
 
     def show
+        @student = Student.find_by(id: params[:id])
     end 
 
     def edit
+        @student = Studentl.find_by(id: params[:id])
     end 
 
     def update
+        @student = Student.find_by(id: params[:id])
+        @student.update(paams.require(:student))
+        redirect_to student_path(@student)
     end 
-    
+
 end 
